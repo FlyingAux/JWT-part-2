e1a9b4206ff0bfc2282720f2f6672e5f1f0c92d1
rajdev04 <rajdevshrivastava72@gmail.com>
2024-06-06 19:32:37 +0530

    mm

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+
+
+app.set('view engine',"ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(cookieParser());
 
