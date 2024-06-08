a886739de27e0a224d73feb7659616589d5daf38
rajdev04 <rajdevshrivastava72@gmail.com>
2024-06-08 14:43:13 +0530

    JWT AUTH COMPELETED

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.get('/',function(req,res,next){
 });
 
 
+app.get('/profile', isLoggedIn ,function(req,res,next){
+    console.log(req.user)
+    res.render('profile');
+});
+
 app.post('/create',function(req,res,next){
     let {username,email,password,age} = req.body;
 
@@ -30,10 +35,48 @@ app.post('/create',function(req,res,next){
                 email,
                 password: hash,
                 age
-            });
+            })
+
+            let token = jwt.sign({email},"shhhhh")
+            res.cookie('token',token);
             res.send(user);
         })
     })
 })
+
+
+app.get('/logout',function(req,res,next){
+    res.cookie('token','')
+    res.redirect('/')
+})
+
+
+app.get('/login',function(req,res,next){
+    res.render('login');
+})
+
+app.post('/login',async function(req,res,next){
+    let user = await userModel.findOne({email: req.body.email});
+        if(!user) return res.send('something went wrong');
+            bcrypt.compare(req.body.password , user.password,function(err,result){
+                if(result) {
+                    let token = jwt.sign({email: user.email},"shhhhh")
+                    res.cookie('token',token);
+                    res.send('you can login');
+                }
+                else res.send('you cant login in fool')
+            })      
+})
+
+
+function isLoggedIn(req,res,next){
+    if(req.cookies.token === '') res.redirect('/login')
+        else{
+            let data = jwt.verify(req.cookies.token , 'shhhhh')
+            req.user = data; 
+        }
+        next();
+}
+
 
 app.listen(3000);
